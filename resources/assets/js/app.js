b6100d197d96d333236d4e1411a57e43dc409f6f
Your Name <you@example.com>
2018-07-23 18:27:34 +0000

    bef Single-file components

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -17,12 +17,19 @@ function escapeKeyListener(evt) {
 }
 
 Vue.component('image-carousel', {
-    template: `<div class="image-carousel">
+    template:
+        `<div class="image-carousel">
 <img v-bind:src="image"/>
     <div class="controls">
-    <carousel-control></carousel-control>
-    <carousel-control></carousel-control>
-    </div>
+<carousel-control
+dir="left"
+@change-image="changeImage"
+></carousel-control>
+<carousel-control
+dir="right"
+@change-image="changeImage"
+></carousel-control>
+</div>
 </div>`,
     props: ['images'],
     data() {
@@ -32,12 +39,41 @@ Vue.component('image-carousel', {
     },
     computed: {
         image() {
+
             return this.images[this.index];
+        }
+    },
+    methods: {
+        changeImage(val) {
+            let newVal = this.index + parseInt(val); //this is calculation adding
+
+            if (newVal < 0) {
+                this.index = this.images.length -1;
+                //if click left too much,display max number index
+                //if continue click, because the newVal is an adding, newVal become positive again
+            } else if (newVal === this.images.length) {
+                this.index = 0;//if  reach max number index+1, display index 0
+            } else {
+                this.index = newVal;//display index
+            }
         }
     },
     components: {
         'carousel-control': {
-            template: `<i class="carousel-control fa fa-2x fa-chevron-left"></i>`
+            template: `<i :class="classes" @click="clicked"></i>`,
+            props: [ 'dir' ],
+            computed: {
+                classes() {
+
+                    return 'carousel-control fa fa-2x fa-chevron-' + this.dir;
+                }
+            },
+            methods: {
+                clicked() {
+
+                    this.$emit('change-image', this.dir === 'left' ? -1 : 1);
+                }
+            }
         }
     }
 });
@@ -70,4 +106,4 @@ var app = new Vue({
     }
 });
 
-console.log('test');
+
